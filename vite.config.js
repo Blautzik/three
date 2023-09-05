cc806623d66e256c92fbbc39cf9325c3050c957b
federico blautzik <fedeblau@gmail.com>
2023-09-05 15:06:18 -0300

    4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-export const pages = {
-    '/otra': {
-        entry: '/otra.html'
+const { resolve } = require('path');
+const { defineConfig } = require('vite');
+const legacy = require('@vitejs/plugin-legacy');
+
+module.exports = defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+          main: resolve(__dirname, 'index.html'),
+          otra: resolve(__dirname, '/otra.html'),
+          otra2: resolve(__dirname, '/otra2.html'),
+      },
     },
-    '/otra2': {
-        entry: '/otra2.html'
-    }
-};
+  },
+});
